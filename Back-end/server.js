7072b621cde3e6d7afc1c2fc9dd878503e1f9da7
Muhammad Bin Ashraf <muhammadashraf342@yahoo.com>
2021-10-05 20:40:07 +0500

    second commit

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 // import products from'./data/products.js'
 import productRoutes from './routes/productRoutes.js'
-
+import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 dotenv.config()
 connectDB()
 // const express =require('express');
@@ -14,6 +14,6 @@ app.get('/',(req,res)=>{
 
 })
 app.use('/api/products' ,productRoutes)
-
-
+app.use(notFound)
+app.use(errorHandler)
 app.listen(5000,console.log('server is running'))
